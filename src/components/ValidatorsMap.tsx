177a2b571cd8967c38b5dfb2fa0eec17e7b2b38a
Nikolay Dorofeev <dorich2000@gmail.com>
2023-10-23 14:28:26 +0400

    Update map params

--- src/components/ValidatorsMap.tsx
+++ src/components/ValidatorsMap.tsx
@@ -4,11 +4,11 @@ import WorldShape from './WorldShape'
 import Validators from './Validators'
 
 export default function ValidatorsMap() {
-  const converter = new MapConverter(1000, 700)
+  const converter = new MapConverter(1000, 600)
 
   return (
     <>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="90 90 910 500" className={styles.map}>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="90 70 910 400" className={styles.map}>
         <WorldShape converter={converter} />
         <Validators mapConverter={converter} />
       </svg>
